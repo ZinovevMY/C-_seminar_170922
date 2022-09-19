3cc53d885eace5c38f1d7320e99cc5d95210aa6c
Mikhail <mehan@list.ru>
2022-09-19 20:32:29 +0300

    Done

--- Homework/Task 1/Program.cs
+++ Homework/Task 1/Program.cs
@@ -1,2 +1,49 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    bool isDigit = int.TryParse(Console.ReadLine(), out int number);
+    if (isDigit)
+    {
+        return number;
+    }
+    throw new Exception("Вы ввели не число");
+}
+
+int[] GenerateArray(int length)
+{
+    int[] answer = new int[length];
+    for (int i = 0; i < answer.Length; i++)
+    {
+        answer[i] = Prompt($"Введите {i+1}-е число: ");
+    }
+    return answer;
+}
+
+void PrintArray(int[] array)
+{
+    foreach (int item in array)
+    {
+        System.Console.Write($"{item}\t");
+    }
+    System.Console.WriteLine();
+}
+
+int PositiveNumberCount(int[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] > 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+int numCount = Prompt("Укажите количество вводимых чисел: ");
+int[] numArray = GenerateArray(numCount);
+PrintArray(numArray);
+System.Console.WriteLine($"Среди введенных чисел {PositiveNumberCount(numArray)} больше 0.");
